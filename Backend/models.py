f3fdb051f1beb10e8181440f4358bec2e4c9b5dd
Sky <skyyounger@gmail.com>
2026-06-10 14:16:13 -0400

    Setup database layer and application model

---
+++ Backend/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class Application(Base):
+    __tablename__ = "applications"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    company = Column(String)
+    job_title = Column(String)
+    status = Column(String)
+    salary = Column(String)
+    location = Column(String)
+    date_applied = Column(String)
+    job_link = Column(String)
+    notes = Column(String)
